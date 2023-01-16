8373dd99499a01a0b53202f044133530b0642b16
Vijaya Vittala M <vvmfunkpac@gmail.com>
2023-01-17 00:10:45 +0530

    new

---
+++ newHashset.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class newHashset {
+    public static void main(String[] args) {
+        HashSet<String> hs=new HashSet<String>(5);
+        hs.add("a");
+        hs.add("b");
+        hs.add("c");
+        hs.add("d");
+//        boolean r2=hs.add("a");
+//        System.out.println(r2);
+        hs.remove("c");
+        System.out.println(hs);
+        System.out.println("C in the collection? "+hs.contains("c"));
+        for(String item : hs){
+            System.out.println(item);
+        }
+    }
+}
